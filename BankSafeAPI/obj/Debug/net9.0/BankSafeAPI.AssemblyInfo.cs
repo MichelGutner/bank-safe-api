03963b8ce439df82ad9076bcaa68b19bf6cb676a
Michel Gutner <michel.gutner@sanar.com>
2025-04-12 16:51:27 -0300

    build: add dependencies

--- BankSafeAPI/obj/Debug/net9.0/BankSafeAPI.AssemblyInfo.cs
+++ BankSafeAPI/obj/Debug/net9.0/BankSafeAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankSafeAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e67930c14a745f437388d143231bd01dcc0afab6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankSafeAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankSafeAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
